3b6211b4dc653072dd829cac6e0488e44d4e525a
Hassaan Ali <imhassaana@gmail.com>
2025-06-12 13:04:36 -0500

    quality of life changes, mainly error handling for auto logout (must use new user/pass feature) and auto redirect to registration page

--- courseTrack.py
+++ courseTrack.py
@@ -6,10 +6,8 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
 import time
-import tkinter as tk
-from tkinter import simpledialog
-
-
+import argparse
+import getpass
 
 def getStatus(SEASON, YEAR, show_gui, refresh_interval):
     login_url = f'https://utdirect.utexas.edu/apps/registrar/course_schedule/{YEAR}{9 if SEASON == "fall" else 2}/{UNIQUE_ID}/'
@@ -19,9 +17,21 @@ def getStatus(SEASON, YEAR, show_gui, refresh_interval):
     driver = webdriver.Chrome(options)
     
     print('Log in and authenticate with Duo')
-    
+
     driver.get(login_url)
     driver.set_window_size(600,600)
+
+    useCreds = input("Would you like to the script to handle credentials? (Still needs Duo) Enter Y/N: ")
+    assert useCreds == 'Y' or useCreds == 'N'
+    useCreds = useCreds == 'Y'
+
+    if(useCreds):
+        username = input("Enter UT EID: ")
+        password = getpass.getpass(prompt="Enter your password: ")
+        driver.find_element(By.ID, "username").send_keys(username)
+        driver.find_element(By.ID, "password").send_keys(password)
+        driver.find_element(By.NAME, "_eventId_proceed").click()
+
 
     WebDriverWait(driver, 60, 0.1).until(EC.presence_of_element_located((By.ID, "registration_info")))
     print("Successfully logged in")
@@ -30,6 +40,18 @@ def getStatus(SEASON, YEAR, show_gui, refresh_interval):
         driver.minimize_window()
 
     while (True):
+        try:
+            WebDriverWait(driver, 10, 0.1).until(EC.presence_of_element_located((By.ID, "registration_info")))
+        except:
+            driver.get(login_url)
+            if(useCreds):
+                driver.find_element(By.ID, "username").send_keys(username)
+                driver.find_element(By.ID, "password").send_keys(password)
+                driver.find_element(By.NAME, "_eventId_proceed").click()
+                WebDriverWait(driver, 20, 0.1).until(EC.presence_of_element_located((By.ID, "registration_info")))
+            else:
+                print("Relogin, page failed to load, most likely auto logout")
+
         className = driver.find_elements(by=By.TAG_NAME, value="h2")[0].text
         matches = driver.find_elements(by=By.TAG_NAME, value="td")
         cur_status = [ x for x in matches if 
@@ -61,28 +83,35 @@ def getStatus(SEASON, YEAR, show_gui, refresh_interval):
         
 def goToRegistrationPage(driver):
     driver.maximize_window()
+    quickRegURL = f'https://utdirect.utexas.edu/registration/registration.WBX?s_ccyys={YEAR}{9 if SEASON == "fall" else 2}&s_af_unique={UNIQUE_ID}'
     registrationURL = "https://utdirect.utexas.edu/registration/chooseSemester.WBX"
     newDriver = driver
-    newDriver.get(registrationURL)
-    
-    # Determine button text based on season and year
-    season_display = "Fall" if SEASON.lower() == "fall" else "Summer" if SEASON.lower() == "spring" else SEASON.capitalize()
-    button_text = f"{season_display} {YEAR} Registration"
-    
-    # Wait for the submit buttons to be present
-    WebDriverWait(newDriver, 20).until(
-        EC.presence_of_element_located((By.XPATH, "//form//input[@name='submit']"))
-    )
-    
-    # Find and click the button that matches the season and year
+
+    newDriver.get(quickRegURL)
+
     try:
-        registration_button = newDriver.find_element(By.XPATH, f"//form//input[@name='submit'][@value='{button_text}']")
-        registration_button.click()
+        WebDriverWait(newDriver, 5, 0.1).until(EC.presence_of_element_located((By.ID, "s_unique_add")))
     except:
-        print(f"Could not find registration button for {button_text}. Attempting to use the first available button.")
-        newDriver.find_element(By.XPATH, "//form//input[@name='submit']").click()
-    
-    WebDriverWait(newDriver, 10).until(EC.presence_of_element_located((By.ID, "s_unique_add")))
+        # Determine button text based on season and year
+        newDriver.get(registrationURL)
+        season_display = "Fall" if SEASON.lower() == "fall" else "Summer" if SEASON.lower() == "spring" else SEASON.capitalize()
+        button_text = f"{season_display} {YEAR} Registration"
+        
+        # Wait for the submit buttons to be present
+        WebDriverWait(newDriver, 20).until(
+            EC.presence_of_element_located((By.XPATH, "//form//input[@name='submit']"))
+        )
+        
+        # Find and click the button that matches the season and year
+        try:
+            registration_button = newDriver.find_element(By.XPATH, f"//form//input[@name='submit'][@value='{button_text}']")
+            registration_button.click()
+        except:
+            print(f"Could not find registration button for {button_text}. Attempting to use the first available button.")
+            newDriver.find_element(By.XPATH, "//form//input[@name='submit']").click()
+        
+        WebDriverWait(newDriver, 10).until(EC.presence_of_element_located((By.ID, "s_unique_add")))
+
     if DROP_ADD:
         drop_dropdown = driver.find_element(By.ID, "s_swap_unique_drop")
         select = Select(drop_dropdown)
@@ -119,24 +148,41 @@ def goToRegistrationPage(driver):
 
 def main():
     global UNIQUE_ID, SWAP_NUMBER, DROP_ADD, DROP_ID, SEASON, YEAR
-    DROP_ADD = simpledialog.askstring("courseTrack", "Would you like to add a class or drop upon successful add? (Enter 'Add' or 'Drop on Add')")
-    assert DROP_ADD == "Add" or DROP_ADD == "Drop on Add"
-    DROP_ADD = DROP_ADD == "Drop on Add"
-    # Create a Tkinter window
-    root = tk.Tk()
-    root.withdraw()  # Hide the main window since we only want the input boxes
-    
-    # Prepare the input fields based on the DROP_ADD flag
-    if DROP_ADD:
-        fieldNames = ["Unique ID to add", "Unique ID to drop", "Unique ID to swap if waitlisted (0 if N/A)", "Season", "Year", "Show display? (Y/N)"]
-    else:
-        fieldNames = ["Unique ID to add", "Unique ID to swap if waitlisted (0 if N/A)", "Season", "Year", "Show display? (Y/N)"]
-    
-    # Ask for the input fields
-    fieldValues = []
-    for field in fieldNames:
-        value = simpledialog.askstring("courseTrack", f"Enter {field}")
-        fieldValues.append(value if value else "")
+    global ADVANCED 
+    ADVANCED = False
+
+    parser = argparse.ArgumentParser(prog='UT Automatic Course Adder', description='A tool to help' \
+            'you automatically check class openings')
+    parser.add_argument('-a', '--advanced', type=str, metavar='',
+            help='Advanced format to enter with Add/Drop, ID to Add, ID to Drop, ' \
+            'ID to Swap for WL (only enter if Drop on Add), Season, Year, Display Y/N. ' \
+            'Example: (enter without quotations) \"Drop 12345 12346 0 fall 2025 Y\" ')
+
+    args = parser.parse_args()
+
+    if args.advanced:
+        ADVANCED = True
+        fieldValues = args.advanced.split()
+        DROP_ADD = fieldValues[0]
+        assert DROP_ADD == "Add" or DROP_ADD == "Drop"
+        DROP_ADD = DROP_ADD == "Drop"
+        del fieldValues[0]
+    else: 
+        DROP_ADD = input("Would you like to add a class or drop upon successful add? (Enter 'Add' or 'Drop') ")
+        assert DROP_ADD == "Add" or DROP_ADD == "Drop"
+        DROP_ADD = DROP_ADD == "Drop"
+        
+        # Prepare the input fields based on the DROP_ADD flag
+        if DROP_ADD:
+            fieldNames = ["Unique ID to add ", "Unique ID to drop ", "Unique ID to swap if waitlisted (0 if N/A) ", "Season ", "Year ", "Show display? (Y/N) "]
+        else:
+            fieldNames = ["Unique ID to add ", "Unique ID to swap if waitlisted (0 if N/A) ", "Season ", "Year ", "Show display? (Y/N) "]
+        
+        # Ask for the input fields
+        fieldValues = []
+        for field in fieldNames:
+            value = input(f"Enter {field}")
+            fieldValues.append(value if value else "")
     
     # Assign input values to corresponding variables
     DROP_ID = 0
@@ -159,14 +205,7 @@ def main():
     assert SEASON == "fall" or SEASON == "spring"
     assert SWAP_NUMBER == 0 or (SWAP_NUMBER >= 10000 and SWAP_NUMBER <= 99999)
     
-    # Schedule the window to close after inputs are gathered
-    root.after(100, root.quit)
-    root.after(100, root.destroy)  # Ensure resources are freed after quitting
-
-    # Start the Tkinter event loop to handle inputs
-    root.mainloop()
     getStatus(SEASON, YEAR, show_gui, refresh_interval)
-
 
 if __name__ == "__main__":
     main()
